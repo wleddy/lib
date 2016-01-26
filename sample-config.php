341dab04c74b4435e4b92ead0fbb5e546d23cad4
Bill Leddy <bill@williesworkshop.net>
2016-01-26 15:00:15 -0800

    Add back the non-sensitive settings

--- sample-config.php
+++ sample-config.php
@@ -2,20 +2,21 @@
 <?php 
 /* Configuration settings for All Sites 
     Update the SMTP credentials below
+    Rename this file to 'config.php
 */
 //echo "root config";
 // set the default time zone
 date_default_timezone_set('America/Los_Angeles');
 
 // Default Email Settings 
-$site['from_name'] = ''; // from email name 
-$site['from_email'] = ''; // from email address 
+$site['from_name'] = 'Bill Leddy'; // from email name 
+$site['from_email'] = 'bill@williesworkshop.net'; // from email address 
 
 //use external mail server. 
 $site['smtp_mode'] = 'enabled'; // enabled or disabled 
-$site['smtp_host'] = ''; 
-$site['smtp_port'] = ''; 
-$site['smtp_username'] = '';
+$site['smtp_host'] = 'smtpout.secureserver.net'; 
+$site['smtp_port'] = '80'; 
+$site['smtp_username'] = 'mailagent@williesworkshop.net';
 $site['smtp_password'] = '';
 
 ?>
