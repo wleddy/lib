dfcf1fe392aca13137ec1e7c72ee807ee3582342
Bill Leddy <bill@williesworkshop.net>
2016-08-21 20:38:27 -0700

    I failed to save the last edits prior to commit

--- configForSite/sample-config.php
+++ configForSite/sample-config.php
@@ -2,12 +2,27 @@
 // Set the path to global settings 
 /*
     This file goes in the same directory as the php mail sending script
+    
+    Rename it to config.php. It will be called by
 */
 
-// DON'T FORGET TO CHANGE THIS...
-// --------------------------------------------------------------------------------------
-$site['config_library'] = '/Users/bleddy/Sites/lib'; // use absolute path to be sure...
 // -------------------------------------------------------------------------------------
+//    Get the path to the library directory dynamically at the default location
+// -------------------------------------------------------------------------------------
+// the trailing slash from $_SERVER['DOCUMENT_ROOT'] is not consistently present 
+//   depending on the server config
+$rootPath = $_SERVER['DOCUMENT_ROOT']."/"; // Ensure there is a trailing slash in Doc Root
+$rootPath = str_replace ("//", "/", $rootPath); //Now ensure that there are not 2 trailing slashes
+
+$site['config_library'] = $rootPath.'../lib'; // always one dir down from Document Root
+
+// ---------------------------------------------------------------------------------------
+//    Or specify the absolute path to the Library directory
+// --------------------------------------------------------------------------------------
+// DON'T FORGET TO CHANGE THIS...
+//$site['config_library'] = '/Users/bleddy/Sites/lib'; // use absolute path to be sure...
+// -------------------------------------------------------------------------------------
+
 
 $site['config_global_settings'] = $site['config_library'].'/config.php'; // Inside the config_library dir
 $site['mailer_class'] = $site['config_library'].'/MailClass.inc'; // Inside the config_library dir
